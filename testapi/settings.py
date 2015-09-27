af489b419be67be0942599dbdbb9755c58bb63df
JackWu <jack.wuweimin@gmail.com>
2015-09-27 12:59:49 -0700

    minior update

--- testapi/settings.py
+++ testapi/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'tastypie',
     'api',
-    'corsheaders'
+
 )
 
 MIDDLEWARE_CLASSES = (
