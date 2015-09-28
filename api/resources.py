b1957cddb205b485ee843ef5db8cb1ee458a38f3
JackWu <jack.wuweimin@gmail.com>
2015-09-27 18:13:22 -0700

    Updated CORS

--- api/resources.py
+++ api/resources.py
@@ -1,13 +1,12 @@
 from tastypie.resources import ModelResource
 from api.models import Customer, Product
-from .corsresource import CorsResourceBase
 
-class CustomerResource(CorsResourceBase, ModelResource):
+class CustomerResource(ModelResource):
 	class Meta:
 		queryset = Customer.objects.all()
 		allowed_methods = ['get']
 
-class ProductResource(CorsResourceBase, ModelResource):
+class ProductResource(ModelResource):
 	class Meta:
 		queryset = Product.objects.all()
 		allowed_method = ['get']
